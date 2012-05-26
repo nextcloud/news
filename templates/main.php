6e91029b210cea92adc3534d9d77e008ced6e99d
Alessandro Cosentino <cosenal@gmail.com>
2012-05-26 15:14:37 -0400

    create opmlimporter class

--- templates/main.php
+++ templates/main.php
@@ -4,9 +4,13 @@ $mapper = new OC_News_FeedMapper();
 $feed = $mapper->fetch( 'http://algorithmsforthekitchen.com/blog/?feed=rss2' );
 echo "<br>" . $feed->getTitle() . "<br>";
 $mapper->insert($feed);
-$feed = $mapper->find($feed->getId());
+$feed = $mapper->findWithItems($feed->getId());
 echo "<br>" . $feed->getTitle() . "<br>";
 $items = $feed->getItems();
+
+foreach($items as $item) {
+	echo $item->getTitle();
+}
 
 $feed2 = $mapper->findWithItems(45);
 echo "<br>" . $feed2->getTitle() . "<br>";
