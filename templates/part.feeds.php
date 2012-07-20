a072943b2e428a14243329ee3717392f7f5e4da1
Gregor Tätzner <gregor@freenet.de>
2012-07-20 11:31:07 +0200

    Polish feedslist layout

--- templates/part.feeds.php
+++ templates/part.feeds.php
@@ -3,10 +3,12 @@
 		$l = new OC_l10n('news');
 
 		echo '<ul class="folders"' . (($depth == 0) ? 'style="margin-left: 0px !important;"' : '') .'> <li class="folder_list" >' .
-			'<div class="collapsable" >' . strtoupper($folder->getName()) .
-                        ( ($depth != 0) ? '<button class="svg action" id="feeds_delete" onClick="(News.Folder.delete(' . $folder->getId(). '))" title="' . $l->t('Delete folder') . '"></button>' .
-				'<button class="svg action" id="feeds_edit" title="' . $l->t('Rename folder') . '"></button>': '' ) .
-                        '</div>';
+			'<div class="folder_container">' .
+				'<div class="collapsable" >' . strtoupper($folder->getName()) .
+				( ($depth != 0) ? '<button class="svg action" id="feeds_delete" onClick="(News.Folder.delete(' . $folder->getId(). '))" title="' . $l->t('Delete folder') . '"></button>' .
+					'<button class="svg action" id="feeds_edit" title="' . $l->t('Rename folder') . '"></button>': '' ) .
+				'</div>' .
+			'</div>';
 		echo '<ul>';
 		$children = $folder->getChildren();
 		foreach($children as $child) {
@@ -15,7 +17,7 @@
 			}
 			elseif ($child instanceOf OC_News_Feed) { //onhover $(element).attr('id', 'newID');
 				$favicon = $child->getFavicon();
-				echo '<li style="background: url(' . $favicon . ') left center no-repeat; background-size:16px 16px;" class="feeds_list" data-id="' . $child->getId() . '"><a href="' . OCP\Util::linkTo('news', 'index.php'). '?feedid=' . $child->getId() . '">' . $child->getTitle() .'</a>';
+				echo '<li class="feeds_list" data-id="' . $child->getId() . '"><a href="' . OCP\Util::linkTo('news', 'index.php'). '?feedid=' . $child->getId() . '" style="background: url(' . $favicon . ') left center no-repeat; background-size:16px 16px;">' . $child->getTitle() .'</a>';
 				countUnreadItems($child->getId());
 				echo '<button class="svg action" id="feeds_delete" onClick="(News.Feed.delete(' . $child->getId(). '))" title="' . $l->t('Delete feed') . '"></button>';
 				echo '<button class="svg action" id="feeds_edit" title="' . $l->t('Edit feed') . '"></button>';
