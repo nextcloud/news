9e1b6c31a8818f665ac58307931c6fd308e6c353
Alessandro Cosentino <cosenal@gmail.com>
2012-08-02 18:39:13 -0400

    moves back the content of templates/listfolder.php

--- templates/part.feeds.php
+++ templates/part.feeds.php
@@ -1,7 +1,17 @@
 <?php
+
 	function print_folder(OC_News_Folder $folder, $depth){
 		$l = new OC_l10n('news');
-		include("part.listfolder.php");
+		echo '<ul class="folders"' . (($depth == 0) ? 'style="margin-left: 0px !important;"' : '') .'> <li class="folder_list" >' .
+			'<div class="collapsable_container" data-id="' . $folder->getId() . '">' .
+				'<div class="collapsable" >' . strtoupper($folder->getName()) .
+				    ( ($depth != 0) ?
+				'<button class="svg action" id="feeds_delete" onClick="(News.Folder.delete(' . $folder->getId(). '))" title="' . $l->t('Delete folder') . '"></button>' .
+				'<button class="svg action" id="feeds_edit" title="' . $l->t('Rename folder') . '"></button>'
+				: '' ) .
+				
+			'</div>' .
+		'<ul>';
 
 		$children = $folder->getChildren();
 		foreach($children as $child) {
