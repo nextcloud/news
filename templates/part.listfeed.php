1117a19bb67188e555e6dcf10a583188c702c3dc
Gregor Tätzner <gregor@freenet.de>
2012-08-02 23:00:14 +0200

    Commit missing files

---
+++ templates/part.listfeed.php
@@ -0,0 +1,18 @@
+<?php
+
+// FIXME: somehow gets included twice
+include_once("part.itemcounter.php");
+
+$child = isset($_['child']) ? $_['child'] : null;
+$favicon = $child->getFavicon();
+if ($favicon == null) {
+	$favicon = OCP\Util::imagePath('news', 'rss.svg');
+}
+
+echo '<li class="feeds_list" data-id="' . $child->getId() . '"><a href="' . OCP\Util::linkTo('news', 'index.php'). '?feedid=' . $child->getId() . '" style="background: url(' . $favicon . ') left center no-repeat; background-size:16px 16px;">' . $child->getTitle() .'</a>';
+countUnreadItems($child->getId());
+echo '<button class="svg action" id="feeds_delete" onClick="(News.Feed.delete(' . $child->getId(). '))" title="' . $l->t('Delete feed') . '"></button>';
+echo '<button class="svg action" id="feeds_edit" title="' . $l->t('Edit feed') . '"></button>';
+echo '</li>';
+
+?>
