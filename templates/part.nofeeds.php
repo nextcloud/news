8bf1873f43b67f4bc6d227639161b4fd001fcd54
Alessandro Cosentino <cosenal@gmail.com>
2012-08-12 14:52:00 -0400

    opml icon in opening page: you know, Sunday jobs :P

--- templates/part.nofeeds.php
+++ templates/part.nofeeds.php
@@ -2,7 +2,7 @@
 <div id="firstrun">
 	<?php echo $l->t('You have no feeds in your reader.') ?>
 	<div id="selections">
-		<input type="button" id="addfeed" value="<?php echo $l->t('Add feed') ?>" />
-		<input type="button" value="<?php echo $l->t('Import OPML') ?>" />
+		<input type="button" id="addfeedbtn" value="<?php echo $l->t('Add feed') ?>" />
+		<input type="button" id="importopmlbtn" value="<?php echo $l->t('Import OPML') ?>" />
 	</div>
 </div>
