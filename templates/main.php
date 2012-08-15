0ca106420e921582459cbaba9b520ed50e7b4a91
Bernhard Posselt <nukeawhale@gmail.com>
2012-08-16 01:17:30 +0200

    renamed subscriptions to new articles

--- templates/main.php
+++ templates/main.php
@@ -12,7 +12,7 @@ echo $this->inc("part.items.header");
 <div id="leftcontent" class="leftcontent">
 	<div id="feeds">
 		<ul>
-			<li data-id="-2" class="subscriptions folder"><a href="#" ><?php echo $l->t('Subscriptions'); ?></a></li>
+			<li data-id="-2" class="subscriptions folder"><a href="#" ><?php echo $l->t('New articles'); ?></a></li>
 			<li data-id="-1" class="starred folder"><a href="#" ><?php echo $l->t('Starred'); ?></a></li>
 			<?php echo $this->inc("part.feeds"); ?>
 		</ul>
