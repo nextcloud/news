fea980d06e9b182bd44a6363c03958db164bd457
Bernhard Posselt <nukeawhale@gmail.com>
2012-10-13 00:06:22 +0200

    added tests page

--- index.php
+++ index.php
@@ -29,53 +29,61 @@ $folderforest = $foldermapper->childrenOf(0); //retrieve all the folders
 $feedid = 0;
 $feedtype = 0;
 
-if ($allfeeds) {
+if(isset($_GET['jstest'])){
+	OCP\Util::addScript('news/3rdparty', 'jasmine-1.2.0/jasmine.js');
+    OCP\Util::addScript('news/3rdparty', 'jasmine-1.2.0/jasmine-html.js');
+    OCP\Util::addStyle('news/3rdparty','jasmine-1.2.0/jasmine.css');
+	$tmpl = new OCP\Template('news', 'javascript.tests');
+	$tmpl->printPage();
+} else {
 
-	OCP\Util::addScript('news','main');
-	OCP\Util::addScript('news','news');
-	OCP\Util::addScript('news','menu');
-	OCP\Util::addScript('news','items');
-	OCP\Util::addScript('news','jquery.timeago');
-	
-	OCP\Util::addStyle('news','news');
-	OCP\Util::addStyle('news','settings');
+	if ($allfeeds) {
 
-	$feedid = isset( $_GET['feedid'] ) ? $_GET['feedid'] : null;
-	if ($feedid == null) {
-		$feedmapper = new OCA\News\FeedMapper(OCP\USER::getUser($userid));
-		$lastViewedId = OCP\Config::getUserValue($userid, 'news', 'lastViewedFeed');
-		$lastViewedType = OCP\Config::getUserValue($userid, 'news', 'lastViewedFeedType');
-		if( $lastViewedId == null || $lastViewedType == null) {
-		    $feedid =  $feedmapper->mostRecent();
-		} else {
-		    $feedid = $lastViewedId;
-		    $feedtype = $lastViewedType;
-		    // check if feed exists in table
-		    if($feedmapper->findById($feedid) === null) {
-				$feedid =  $feedmapper->mostRecent();
-		    }
+		OCP\Util::addScript('news','main');
+		OCP\Util::addScript('news','news');
+		OCP\Util::addScript('news','menu');
+		OCP\Util::addScript('news','items');
+		OCP\Util::addScript('news/3rdparty', 'jquery.timeago');
+		
+		OCP\Util::addStyle('news','news');
+		OCP\Util::addStyle('news','settings');
+
+		$feedid = isset( $_GET['feedid'] ) ? $_GET['feedid'] : null;
+		if ($feedid == null) {
+			$feedmapper = new OCA\News\FeedMapper(OCP\USER::getUser($userid));
+			$lastViewedId = OCP\Config::getUserValue($userid, 'news', 'lastViewedFeed');
+			$lastViewedType = OCP\Config::getUserValue($userid, 'news', 'lastViewedFeedType');
+			if( $lastViewedId == null || $lastViewedType == null) {
+			    $feedid =  $feedmapper->mostRecent();
+			} else {
+			    $feedid = $lastViewedId;
+			    $feedtype = $lastViewedType;
+			    // check if feed exists in table
+			    if($feedmapper->findById($feedid) === null) {
+					$feedid =  $feedmapper->mostRecent();
+			    }
+			}
 		}
+		$tmpl = new OCP\Template( 'news', 'main', 'user' );
+		$tmpl->assign('allfeeds', $allfeeds);
+		$tmpl->assign('folderforest', $folderforest);
+		$tmpl->assign('feedid', $feedid);
+		$tmpl->assign('feedtype', $feedtype);
+		$tmpl->printPage();
+
+	} else {
+
+		OCP\Util::addScript('news','main');
+		OCP\Util::addScript('news','news');
+		OCP\Util::addScript('news','menu');
+		OCP\Util::addScript('news','items');
+		
+		OCP\Util::addStyle('news','firstrun');
+
+		$tmpl = new OCP\Template( 'news', 'firstrun', 'user' );
+		$tmpl->printPage();
+
 	}
-	$tmpl = new OCP\Template( 'news', 'main', 'user' );
-	$tmpl->assign('allfeeds', $allfeeds);
-	$tmpl->assign('folderforest', $folderforest);
-	$tmpl->assign('feedid', $feedid);
-	$tmpl->assign('feedtype', $feedtype);
-	$tmpl->printPage();
+
 
 }
-else {
-
-	OCP\Util::addScript('news','main');
-	OCP\Util::addScript('news','news');
-	OCP\Util::addScript('news','menu');
-	OCP\Util::addScript('news','items');
-	
-	OCP\Util::addStyle('news','firstrun');
-
-	$tmpl = new OCP\Template( 'news', 'firstrun', 'user' );
-	$tmpl->printPage();
-
-}
-
-
