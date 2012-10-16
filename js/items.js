bf83f65545f68515cc75c103b4b6cdaccad8e427
Bernhard Posselt <nukeawhale@gmail.com>
2012-10-16 18:45:49 +0200

    click on from_feed loads the feed

--- js/items.js
+++ js/items.js
@@ -605,6 +605,12 @@ var News = News || {};
             self.setRead(true);
         });
 
+        // bind click to load feed when the from_feed link is viewable
+        this._$html.find('.from_feed').click(function(){
+            News.Objects.Items.load(News.MenuNodeType.Feed, self.getFeedId(), function(){});
+            return false;
+        });
+
         // single hover on item should mark it as read too
         this._$html.find('.body').click(function(){
             var $item = $(this).parent('.feed_item');
--- templates/part.items.php
+++ templates/part.items.php
@@ -34,7 +34,7 @@ foreach($items as $item) {
 		echo '<h1 class="item_title"><a target="_blank" href="' . $item->getUrl() . '">' . htmlspecialchars($item->getTitle(), ENT_QUOTES, 'UTF-8') . '</a></h1>';
 
 		if ((int)$lastViewedFeedType !== OCA\News\FeedType::FEED) {
-			$feedTitle = '<span class="from_feed"> ' . $l->t('from') . ' ' . $item->getFeedTitle() . '</span> ';
+			$feedTitle = '<a href="#" class="from_feed"> ' . $l->t('from') . ' ' . $item->getFeedTitle() . '</a> ';
 		} else {
 			$feedTitle = '';
 		}
