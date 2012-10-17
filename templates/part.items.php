138de85e03495a08250773541ab74b3410f6ce8f
Alessandro Cosentino <cosenal@gmail.com>
2012-10-17 16:31:44 -0400

    move the from out of the link

--- templates/part.items.php
+++ templates/part.items.php
@@ -34,7 +34,7 @@ foreach($items as $item) {
 		echo '<h1 class="item_title"><a target="_blank" href="' . $item->getUrl() . '">' . htmlspecialchars($item->getTitle(), ENT_QUOTES, 'UTF-8') . '</a></h1>';
 
 		if ((int)$lastViewedFeedType !== OCA\News\FeedType::FEED) {
-			$feedTitle = '<a href="#" class="from_feed"> ' . $l->t('from') . ' ' . $item->getFeedTitle() . '</a> ';
+			$feedTitle = $l->t('from') . ' ' . '<a href="#" class="from_feed"> ' . $item->getFeedTitle() . '</a> ';
 		} else {
 			$feedTitle = '';
 		}
