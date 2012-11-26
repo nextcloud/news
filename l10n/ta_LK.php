2d9e51614f5c5fe7bdad0f3528b451d9aa773c74
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-11-27 00:09:28 +0100

    [tx-robot] updated from transifex

--- l10n/ta_LK.php
+++ l10n/ta_LK.php
@@ -1,10 +1,18 @@
 <?php $TRANSLATIONS = array(
 "Error" => "வழு",
 "Warning" => "எச்சரிக்கை",
+"Address" => "முகவரி",
 "Upload" => "பதிவேற்றுக",
 "Import" => "இறக்குமதி",
 "Folder" => "கோப்புறை",
 "Settings" => "அமைப்புகள்",
+"Add Folder" => "கோப்புறையை சேர்க்க",
+"Folder name" => "கோப்புறை பெயர்",
+"Choose folder" => "கோப்புறையை தெரிவுசெய்க",
 "Add" => "சேர்க்க",
-"Share" => "பகிர்வு"
+"Mark as unimportant" => "முக்கியமற்றதாக குறிப்பிடுக",
+"Mark as important" => "முக்கியமானதாக குறிப்பிடுக",
+"by" => "இனால்",
+"Share" => "பகிர்வு",
+"Keep unread" => "வாசிக்கப்படாததை வைக்க"
 );
