d4064fe2c7c1b5a6eb89714503c0fe775729d8af
Bernhard Posselt <nukeawhale@gmail.com>
2013-02-02 00:23:55 +0100

    added annotations test

--- tests/controller/FolderControllerTest.php
+++ tests/controller/FolderControllerTest.php
@@ -26,12 +26,13 @@
 namespace OCA\News\Controller;
 
 use \OCA\AppFramework\Http\Request;
+use OCA\AppFramework\Utility\ControllerTestUtility;
 
 
 require_once(__DIR__ . "/../classloader.php");
 
 
-class FolderControllerTest extends \PHPUnit_Framework_TestCase {
+class FolderControllerTest extends ControllerTestUtility {
 
 	private $api;
 	private $folderMapper;
@@ -43,8 +44,7 @@ class FolderControllerTest extends \PHPUnit_Framework_TestCase {
 	 * Gets run before each test
 	 */
 	public function setUp(){
-		$this->api = $this->getMock('\OCA\AppFramework\Core\API', 
-			null, array('news'));
+		$this->api = $this->getAPIMock();
 		$this->folderMapper = $this->getMock('FolderMapper',
 			array('getAll'));
 		$this->request = new Request();
@@ -74,6 +74,14 @@ class FolderControllerTest extends \PHPUnit_Framework_TestCase {
 
 		$response = $this->controller->getAll();
 		$this->assertEquals($return, $response->getParams());
+	}
+
+
+	public function testGetAllAnnotations(){
+		$methodName = 'getAll';
+		$annotations = array('IsAdminExemption', 'IsSubAdminExemption');
+
+		$this->assertAnnotations($this->controller, $methodName, $annotations);
 	}
 
 
