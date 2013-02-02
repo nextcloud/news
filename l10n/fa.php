239f1bce8c249264346d1f264cda652a271e8b4e
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-02-03 00:04:16 +0100

    [tx-robot] updated from transifex

--- l10n/fa.php
+++ l10n/fa.php
@@ -1,6 +1,17 @@
 <?php $TRANSLATIONS = array(
+"year" => "سال",
+"News" => "اخبار",
+"no title" => "بدون عنوان",
+"no name" => "بدون نام",
 "Share" => "اشتراک‌گزاری",
+"Delete folder" => "حذف پوشه",
+"Rename folder" => "تغییر نام پوشه",
+"-- choose folder --" => "-- انتخاب پوشه --",
 "Address" => "آدرس",
 "Add" => "افزودن",
+"Add Folder" => "ایجاد پوشه",
+"Folder name must not be empty!" => "نام پوشه نباید خالی باشد!",
+"Folder Name" => "نام پوشه",
+"Show everything" => "نمایش همه",
 "Settings" => "تنظیمات"
 );
