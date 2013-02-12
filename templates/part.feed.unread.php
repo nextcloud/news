f9f627bd7482fd8390407bc9989e27856c2705af
Bernhard Posselt <nukeawhale@gmail.com>
2013-02-12 01:35:14 +0100

    show unread and all now change the caption of the feed that shows all items

--- templates/part.feed.unread.php
+++ templates/part.feed.unread.php
@@ -5,8 +5,15 @@
 	ng-show="isShown(feedType.Subscriptions, 0)">
 	<a class="rss-icon" 
 	   href="#" 
+	   ui-if="getShowAll()==false"
 	   ng-click="loadFeed(feedType.Subscriptions, 0)">
-	   <?php p($l->t('New articles'))?>
+	   <?php p($l->t('Unread articles'))?>
+	</a>
+		<a class="rss-icon" 
+	   href="#" 
+	   ui-if="getShowAll()==true"
+	   ng-click="loadFeed(feedType.Subscriptions, 0)">
+	   <?php p($l->t('All articles'))?>
 	</a>
 	<span class="utils">
 		<button class="svg action mark-read-icon" 
