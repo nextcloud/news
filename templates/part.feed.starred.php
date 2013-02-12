5fa3d41316a583c4d436b327a0d2044093dffa12
Bernhard Posselt <nukeawhale@gmail.com>
2013-02-12 01:29:09 +0100

    brought back unread and starred feeds

--- templates/part.feed.starred.php
+++ templates/part.feed.starred.php
@@ -1,17 +1,8 @@
-<li ng-class="{
-		active: isFeedActive(feedType.Starred, 0),
-		all_read: getUnreadCount(feedType.Starred, 0)==0
-	}" 
-	class="starred-icon"
+<li ng-class="{	active: isFeedActive(feedType.Starred, 0) }" 
 	ng-show="isShown(feedType.Starred, 0)">
-	<a class="title" 
-	   href="#"
-	   ng-click="loadFeed(feedType.Starred, 0)">
+	<a class="starred-icon"
+		href="#"
+		ng-click="loadFeed(feedType.Starred, 0)">
 	   <?php p($l->t('Starred')) ?>
 	</a>
-	<span class="utils">
-		<span class="unread_items_counter">
-			{{ getUnreadCount(feedType.Starred, 0) }}
-		</span>
-	</span>
 </li>
--- templates/part.feed.unread.php
+++ templates/part.feed.unread.php
@@ -1,20 +1,19 @@
 <li ng-class="{
 		active: isFeedActive(feedType.Subscriptions, 0),
-		all_read: getUnreadCount(feedType.Subscriptions, 0)==0
+		unread: getUnreadCount(feedType.Subscriptions, 0)!=0
 	}" 
-	class="subscriptions"
 	ng-show="isShown(feedType.Subscriptions, 0)">
-	<a class="title" 
+	<a class="rss-icon" 
 	   href="#" 
 	   ng-click="loadFeed(feedType.Subscriptions, 0)">
 	   <?php p($l->t('New articles'))?>
 	</a>
 	<span class="utils">
-		<span class="unread-counter">
-			{{ getUnreadCount(feedType.Starred, 0) }}
-		</span>
 		<button class="svg action mark-read-icon" 
 			ng-click="markAllRead(feedType.Subscriptions, 0)"
 			title="<?php p($l->t('Mark all read')) ?>"></button>
+		<span class="unread-counter">
+			{{ getUnreadCount(feedType.Subscriptions, 0) }}
+		</span>
 	</span>
 </li>
