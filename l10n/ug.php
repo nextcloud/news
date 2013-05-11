c620c825d8132690a7b51242501a9e5c63f5b32f
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-05-12 01:03:16 +0200

    [tx-robot] updated from transifex

---
+++ l10n/ug.php
@@ -0,0 +1,17 @@
+<?php $TRANSLATIONS = array(
+"News" => "خەۋەر",
+"Address" => "ئادرېس",
+"Add" => "قوش",
+"New folder" => "يېڭى قىسقۇچ",
+"Folder" => "قىسقۇچ",
+"Folder name" => "قىسقۇچ ئاتى",
+"Create folder" => "قىسقۇچ قۇر",
+"Starred" => "يۇلتۇزلۇق",
+"Mark read" => "ئوقۇلدى بەلگىسى قوي",
+"by" => "سەنئەتكار",
+"Delete folder" => "قىسقۇچ ئۆچۈر",
+"Rename folder" => "قىسقۇچ ئاتىنى ئۆزگەرت",
+"Import" => "ئەكىر",
+"Export" => "چىقار",
+"Show all" => "ھەممىنى كۆرسەت"
+);
