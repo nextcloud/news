4929ce03a7d9e94f353e5a35d5c483691a11c616
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-05-14 01:03:02 +0200

    [tx-robot] updated from transifex

--- l10n/pt_PT.php
+++ l10n/pt_PT.php
@@ -3,6 +3,7 @@
 "Can not add feed: Exists already" => "Não é possível adicionar o feed: Já existe",
 "Can not add feed: URL does not exist or has invalid xml" => "Não é possível adicionar o feed: o endereço não existe ou contem um xml inválido",
 "Can not add folder: Exists already" => "Não é possivel adicionar a pasta: Já existe",
+"Undo deletion of %s" => "Anular o apagar de %s",
 "Add Website" => "Adicionar WebSite",
 "Error: address exists already!" => "Erro: o endereço já existe!",
 "Error: folder exists already" => "Erro: a pasta já existe",
@@ -17,6 +18,7 @@
 "Starred" => "Marcado",
 "Unread articles" => "Artigos por ler",
 "All articles" => "Todos os artigos",
+"Mark read" => "Seleccionar leitura",
 "from" => "de",
 "by" => "por",
 "Cant play audio format" => "Não é possível reproduzir este formato de audio",
@@ -29,6 +31,9 @@
 "Import" => "Importar",
 "Export" => "Exportar",
 "Error when importing: file does not contain valid OPML" => "Erro na importação: o ficheiro não contém um OPML válido",
+"Import Google Reader JSON" => "Importar Leitor Google JSON",
+"To import starred and shared articles from Google \n\t\t\tReader please upload the .json files from the Google Takeout archive" => "Para importar artigos marcados e partilhados da Google\n»»»Leitor por favor envie os ficheiros .json para o arquivo Google Takeout",
+"Error when importing: file does not contain valid JSON" => "Erro ao importar: ficheiro não contém JSON válido",
 "Show all" => "Mostrar todos",
 "Show only unread" => "Mostrar apenas não lidas"
 );
