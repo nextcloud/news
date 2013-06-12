84a387619ca8aaa58703d69a0d008188dd7f4592
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-06-13 01:05:17 +0200

    [tx-robot] updated from transifex

---
+++ l10n/bs.php
@@ -0,0 +1,13 @@
+<?php $TRANSLATIONS = array(
+"News" => "Vijesti",
+"Address" => "Adresa",
+"Add" => "Dodaj",
+"New folder" => "Nova fascikla",
+"Folder" => "Fasikla",
+"Starred" => "Označeno",
+"Keep unread" => "Zadrži nepročitano",
+"Collapse" => "Proširi",
+"Delete folder" => "Izbriši fasciklu",
+"Rename folder" => "Preimenuj fasciklu",
+"Show only unread" => "Prikaži samo nepročitane"
+);
