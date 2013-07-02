5fe4ef46b8c99cf8672b539141e9c7db7df23dd6
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-07-02 15:40:36 +0200

    [tx-robot] updated from transifex

--- l10n/te.php
+++ l10n/te.php
@@ -1,3 +1,16 @@
 <?php $TRANSLATIONS = array(
-"Add" => "చేర్చు"
+"News" => "వార్తలు",
+"Address" => "చిరునామా",
+"Add" => "చేర్చు",
+"New folder" => "కొత్త సంచయం",
+"Folder" => "సంచయం",
+"Folder name" => "సంచయం పేరు",
+"Unread articles" => "చదవని వ్యాసాలు",
+"All articles" => "అన్ని వ్యాసాలు",
+"Delete folder" => "సంచయాన్ని తొలగించు",
+"Rename folder" => "సంచయం పేరుమార్చు",
+"Import" => "దిగుమతించు",
+"Export" => "ఎగుమతించు",
+"Show all" => "అన్నీ చూపించు",
+"Show only unread" => "చదవనివి మాత్రమే చూపించు"
 );
