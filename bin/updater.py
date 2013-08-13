1d87bb43c7439ea9f91d652f417e17457d0311df
Bernhard Posselt <nukeawhale@gmail.com>
2013-08-13 13:59:07 +0200

    Update updater.py

--- bin/updater.py
+++ bin/updater.py
@@ -132,7 +132,7 @@ class Updater:
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--runonce',
-        help='Run update only once, only recommended for testing', action='store_true')
+        help='Run update only once, DO NOT use this in a cron job, only recommended for testing', action='store_true')
     parser.add_argument('--threads',
         help='How many feeds should be fetched in paralell, defaults to 10',
         default=10,
