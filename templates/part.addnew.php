d8eb445220de1b6a71ce77468b6aedec3e547697
Bernhard Posselt <dev@bernhard-posselt.com>
2013-09-23 01:51:17 +0200

    add form shouldn’t be blocked while adding a feed Edit

--- templates/part.addnew.php
+++ templates/part.addnew.php
@@ -29,12 +29,11 @@
 				<input type="text" 
 					ng-model="feedUrl" 
 					placeholder="<?php p($l->t('Address')); ?>" 
-					ng-disabled="isAddingFeed() || isAddingFolder()"
 					name="adress"
 					autofocus>
 				<button title="<?php p($l->t('Add')); ?>" 
 						class="primary"
-						ng-disabled="isAddingFeed() || isAddingFolder() || !feedUrl.trim()"
+						ng-disabled="!feedUrl.trim()"
 						ng-click="addFeed(feedUrl, folderId.id)"><?php p($l->t('Add')); ?></button>
 			</form>
 			<form>
@@ -42,7 +41,6 @@
 						data-create="<?php p($l->t('New folder')); ?>"
 						title="<?php p($l->t('Folder')); ?>"		
 						ng-model="folderId"
-						ng-disabled="isAddingFolder()"
 						ng-options="folder.name for folder in folderBusinessLayer.getAll()"
 						ng-hide="addNewFolder">
 					<option value="" selected="selected"><?php p($l->t('Choose folder')); ?></option>
@@ -54,7 +52,6 @@
 						oc-click-focus="{selector: 'input[name=\'foldername\']'}"></button>
 				<input type="text" 
 						ng-model="folderName" 
-						ng-disabled="isAddingFolder()"
 						ng-show="addNewFolder"
 						name="foldername"
 						placeholder="<?php p($l->t('Folder name')); ?>" 
@@ -63,12 +60,11 @@
 				<button title="<?php p($l->t('Back to folder selection')); ?>" 
 						ng-show="addNewFolder"
 						ng-click="addNewFolder=false"
-						ng-disabled="isAddingFolder()"
 						class="action-button back-button action"></button>
 				<button title="<?php p($l->t('Create folder')); ?>" 
 						ng-show="addNewFolder"
 						ng-click="addFolder(folderName)"
-						ng-disabled="isAddingFolder() || !folderName.trim()"
+						ng-disabled="!folderName.trim()"
 						ng-class="{loading: isAddingFolder()}"
 						class="action-button create-button action">
 				</button>
