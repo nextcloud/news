d3f1b68660488f6d77d600c71c923a021977b06a
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-10-19 08:16:44 -0400

    [tx-robot] updated from transifex

--- l10n/ru_RU.php
+++ l10n/ru_RU.php
@@ -1,16 +1,3 @@
 <?php $TRANSLATIONS = array(
-"News" => "Новости",
-"Address" => "Адрес",
-"Add" => "Добавить",
-"Folder" => "Папка",
-"Folder name" => "Имя папки",
-"Starred" => "Отмечено звездочкой",
-"by" => " ",
-"Keep unread" => "Оставить непрочитанным",
-"Collapse" => "Свернуть",
-"Delete folder" => "Удалить папку",
-"Rename folder" => "Переименовать папку",
-"Import" => "Импортировать",
-"Export" => "Экспортировать",
-"Show only unread" => "Показать только непрочитанное"
+"Download" => "Загрузка"
 );
