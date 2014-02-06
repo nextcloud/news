acec12a5dc6de8e3427165c81f9c6a79f4355e9b
Davide Saurino <davide.saurino@alcacoop.it>
2014-02-06 16:38:14 +0100

    Rename folders (FIX #456)

--- templates/part.listfolder.php
+++ templates/part.listfolder.php
@@ -11,10 +11,22 @@
 	data-id="{{ folder.id }}"
 	droppable>
 	<button class="collapse" 
+			ng-hide="folder.editing"
 			title="<?php p($l->t('Collapse'));?>"
 			ng-click="folderBusinessLayer.toggleFolder(folder.id)"></button>
+	<div ui-if="folder.editing" class="rename-feed">
+          <input type="text" ng-model="folder.name" autofocus>
+          <button title="<?php p($l->t('Cancel')); ?>"
+		ng-click="folderBusinessLayer.cancel(folder.id)"
+		class="action-button back-button action"></button>
+	  <button title="<?php p($l->t('Save')); ?>"
+		ng-click="folderBusinessLayer.rename(folder.id, folder.name)"
+		class="action-button create-button action">
+	  </button>
+        </div>
 	<a href="#" 
 	   class="title folder-icon"
+	   ng-hide="folder.editing"
 	   ng-click="folderBusinessLayer.load(folder.id)"
 	   ng-class="{
 			'progress-icon': !folder.id,
@@ -27,7 +39,7 @@
 	<span class="utils">
 
 		<button ng-click="folderBusinessLayer.delete(folder.id)"
-				ng-hide="!folder.id"
+				ng-hide="folder.editing || !folder.id"
 				class="svg action delete-icon delete-button" 
 				title="<?php p($l->t('Delete folder')); ?>"
 				oc-tooltip></button>
@@ -49,10 +61,11 @@
 			ng-show="folder.error"
 			oc-tooltip></button>
 
-<!--		<button class="svg action edit-icon" 
-				ng-click="renameFolder(folder.id)"
-				title="<?php p($l->t('Rename folder')); ?>"></button>
--->
+		<button class="svg action rename-feed-icon"
+	                ng-hide="folder.editing"
+			ng-click="folderBusinessLayer.edit(folder.id)"
+			title="<?php p($l->t('Rename folder')); ?>"
+	                oc-tooltip></button>
 
 	</span>
 	<ul>
