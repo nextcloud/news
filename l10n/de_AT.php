61aaacaf63f2166922764567e937c8f3809c7038
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-02-15 06:15:22 -0500

    [tx-robot] updated from transifex

---
+++ l10n/de_AT.php
@@ -0,0 +1,9 @@
+<?php $TRANSLATIONS = array(
+"from" => "von",
+"by" => "von",
+"Download" => "Herunterladen",
+"Cancel" => "Abbrechen",
+"Save" => "Speichern",
+"Settings" => "Einstellungen",
+"Import" => "Importieren"
+);
