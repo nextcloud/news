6e0de4208c06a3c95cbe274a93476b6b7eacafa0
Bernhard Posselt <dev@bernhard-posselt.com>
2014-04-09 16:36:09 +0200

    remove usersettingscontroller in favor of pagecontroller, simplify settings requests

--- controller/pagecontroller.php
+++ controller/pagecontroller.php
@@ -26,15 +26,18 @@
 namespace OCA\News\Controller;
 
 use \OCP\IRequest;
+use \OCP\AppFramework\Http\JSONResponse;
 use \OCP\AppFramework\Controller;
 
 use \OCA\News\Core\API;
 
 class PageController extends Controller {
 
+	private $api;
 
 	public function __construct(API $api, IRequest $request){
 		parent::__construct($api->getAppName(), $request);
+		$this->api = $api;
 	}
 
 
@@ -46,5 +49,35 @@ class PageController extends Controller {
 		return $this->render('main');
 	}
 
+
+	/**
+	 * @NoAdminRequired
+	 */
+	public function settings() {
+		$showAll = $this->api->getUserValue('showAll');
+		$compact = $this->api->getUserValue('compact');
+		$language = $this->api->getTrans()->findLanguage();
+
+		$settings = array(
+			'showAll' => $showAll === '1',
+			'compact' => $compact === '1',
+			'language' => $language
+		);
+
+		return new JSONResponse($settings);
+	}
+
+
+	/**
+	 * @NoAdminRequired
+	 */
+	public function updateSettings() {
+		$isShowAll = $this->params('showAll');
+		$isCompact = $this->params('compact');
+		$this->api->setUserValue('showAll', $isShowAll);
+		$this->api->setUserValue('compact', $isCompact);
+
+		return new JSONResponse();
+	}
 
 }
--- tests/unit/controller/PageControllerTest.php
+++ tests/unit/controller/PageControllerTest.php
@@ -27,6 +27,7 @@ namespace OCA\News\Controller;
 
 use \OCP\IRequest;
 use \OCP\AppFramework\Http\TemplateResponse;
+use \OCP\AppFramework\Http\JSONResponse;
 
 use \OCA\News\Utility\ControllerTestUtility;
 
@@ -38,6 +39,7 @@ class PageControllerTest extends ControllerTestUtility {
 	private $api;
 	private $request;
 	private $controller;
+	private $user;
 
 
 	/**
@@ -47,12 +49,23 @@ class PageControllerTest extends ControllerTestUtility {
 		$this->api = $this->getAPIMock();
 		$this->request = $this->getRequest();
 		$this->controller = new PageController($this->api, $this->request);
+		$this->user = 'becka';
 	}
 
 
-	public function testOpmlAnnotations(){
+	public function testIndexAnnotations(){
 		$annotations = array('NoAdminRequired', 'NoCSRFRequired');
 		$this->assertAnnotations($this->controller, 'index', $annotations);
+	}
+
+	public function testSettingsAnnotations(){
+		$annotations = array('NoAdminRequired');
+		$this->assertAnnotations($this->controller, 'settings', $annotations);
+	}
+
+	public function testUpdateSettingsAnnotations(){
+		$annotations = array('NoAdminRequired');
+		$this->assertAnnotations($this->controller, 'updateSettings', $annotations);
 	}
 
 	public function testIndex(){
@@ -62,4 +75,52 @@ class PageControllerTest extends ControllerTestUtility {
 	}
 
 
+	public function testSettings() {
+		$result = array(
+			'showAll' => true,
+			'compact' => true,
+			'language' => 'de'
+		);
+
+		$lang = $this->getMock('Lang', array('findLanguage'));
+		$lang->expects($this->once())
+			->method('findLanguage')
+			->will($this->returnValue('de'));
+		$this->api->expects($this->once())
+			->method('getTrans')
+			->will($this->returnValue($lang));
+		$this->api->expects($this->at(0))
+			->method('getUserValue')
+			->with($this->equalTo('showAll'))
+			->will($this->returnValue('1'));
+		$this->api->expects($this->at(1))
+			->method('getUserValue')
+			->with($this->equalTo('compact'))
+			->will($this->returnValue('1'));
+
+		$response = $this->controller->settings();
+		$this->assertEquals($result, $response->getData());
+		$this->assertTrue($response instanceof JSONResponse);
+	}
+
+
+	public function testUpdateSettings() {
+		$request = $this->getRequest(array('post' => array(
+			'showAll' => true,
+			'compact' => true
+		)));
+		$this->controller = new PageController($this->api, $request);
+
+		$this->api->expects($this->at(0))
+			->method('setUserValue')
+			->with($this->equalTo('showAll'), 
+				$this->equalTo(true));
+		$this->api->expects($this->at(1))
+			->method('setUserValue')
+			->with($this->equalTo('compact'), 
+				$this->equalTo(true));
+		$response = $this->controller->updateSettings();
+
+		$this->assertTrue($response instanceof JSONResponse);
+	}
 }
