2b6a507df5e0478399a574b328104a56a90ff8f3
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-27 01:01:09 -0400

    [tx-robot] updated from transifex

--- l10n/zh_HK.php
+++ l10n/zh_HK.php
@@ -2,10 +2,12 @@
 "Address" => "地址",
 "Add" => "加入",
 "New folder" => "新文件夾",
+"Folder" => "資料夾",
 "Folder name" => "資料夾名稱",
 "Download" => "下載",
 "Cancel" => "取消",
 "Save" => "儲存",
 "Settings" => "設定",
-"Import" => "導入"
+"Import" => "導入",
+"Export" => "輸出"
 );
