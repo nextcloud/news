e9c9ea59e4cd8bbcfe7e1a8783f7b1a9aa64ab54
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-17 01:01:27 -0400

    [tx-robot] updated from transifex

---
+++ l10n/az.php
@@ -0,0 +1,12 @@
+<?php $TRANSLATIONS = array(
+"Can not add feed: Exists already" => "Ötürməni əlavə etmək olmadı: Artıq mövcuddur",
+"Can not add feed: URL does not exist, SSL Certificate can not be validated or feed has invalid xml" => "Ötürücünü əlavə etmək mümkün olmadı: URL mövcud deyil, SSL sertifikat yoxlanıla bilmir yada ötürücüdə XML yalnışdır",
+"Articles without feed" => "Ötürməsiz məqalələr",
+"Can not add folder: Exists already" => "Qovluğu əlave etmək mümkün deyil: Artıq movcuddur",
+"_" => "_",
+"Cancel" => "Dayandır",
+"Save" => "Saxlamaq",
+"Settings" => "Quraşdırmalar",
+"Import" => "Əlavə et",
+"Export" => "Çıxarış"
+);
