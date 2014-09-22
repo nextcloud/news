d2fd0282a3924b8f62d45f5746e639fac9af0549
Bernhard Posselt <dev@bernhard-posselt.com>
2014-09-22 16:06:17 +0200

    dont backport component

--- templates/7.php
+++ templates/7.php
@@ -35,20 +35,3 @@ function style($app, $file) {
 	}
 }
 
-/**
- * Shortcut for HTML imports
- * @param string $app the appname
- * @param string|string[] $file the path relative to the app's component folder,
- * if an array is given it will add all components
- */
-function component($app, $file) {
-	if(is_array($file)) {
-		foreach($file as $f) {
-			$url = link_to($app, 'component/' . $f . '.html');
-			\OCP\Util::addHeader('link', array('rel' => 'import', 'href' => $url));
-		}
-	} else {
-		$url = link_to($app, 'component/' . $file . '.html');
-		\OCP\Util::addHeader('link', array('rel' => 'import', 'href' => $url));
-	}
-}
