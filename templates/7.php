cfd8513e97ad72c173b6fa613634678892b66401
Bernhard Posselt <dev@bernhard-posselt.com>
2014-09-22 16:05:47 +0200

    use public util for backport

--- templates/7.php
+++ templates/7.php
@@ -12,10 +12,10 @@
 function script($app, $file) {
 	if(is_array($file)) {
 		foreach($file as $f) {
-			OC_Util::addScript($app, $f);
+			\OCP\Util::addScript($app, $f);
 		}
 	} else {
-		OC_Util::addScript($app, $file);
+		\OCP\Util::addScript($app, $file);
 	}
 }
 
@@ -28,10 +28,10 @@ function script($app, $file) {
 function style($app, $file) {
 	if(is_array($file)) {
 		foreach($file as $f) {
-			OC_Util::addStyle($app, $f);
+			\OCP\Util::addStyle($app, $f);
 		}
 	} else {
-		OC_Util::addStyle($app, $file);
+		\OCP\Util::addStyle($app, $file);
 	}
 }
 
@@ -45,10 +45,10 @@ function component($app, $file) {
 	if(is_array($file)) {
 		foreach($file as $f) {
 			$url = link_to($app, 'component/' . $f . '.html');
-			OC_Util::addHeader('link', array('rel' => 'import', 'href' => $url));
+			\OCP\Util::addHeader('link', array('rel' => 'import', 'href' => $url));
 		}
 	} else {
 		$url = link_to($app, 'component/' . $file . '.html');
-		OC_Util::addHeader('link', array('rel' => 'import', 'href' => $url));
+		\OCP\Util::addHeader('link', array('rel' => 'import', 'href' => $url));
 	}
 }
