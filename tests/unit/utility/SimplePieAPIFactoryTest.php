d9920cc529720f39ed152b06470c44bdfa3ca755
Bernhard Posselt <dev@bernhard-posselt.com>
2014-10-08 23:09:00 +0200

    add test for simplepie api factory

---
+++ tests/unit/utility/SimplePieAPIFactoryTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * ownCloud - News
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Alessandro Cosentino <cosenal@gmail.com>
+ * @author Bernhard Posselt <dev@bernhard-posselt.com>
+ * @copyright Alessandro Cosentino 2012
+ * @copyright Bernhard Posselt 2012, 2014
+ */
+
+namespace OCA\News\Utility;
+
+
+class SimplePieAPIFactoryTest extends \PHPUnit_Framework_TestCase {
+
+
+	public function testGetFile() {
+		$factory = new SimplePieAPIFactory();
+		$file = $factory->getFile('php://input', 10, 5, $headers='headers',
+	                        $useragent='flashce', $force_fsockopen=true);
+		$this->assertTrue($file instanceof \SimplePie_File);
+	}
+
+
+	public function testGetCore() {
+		$factory = new SimplePieAPIFactory();
+		$this->assertTrue($factory->getCore() instanceof \SimplePie);
+	}
+
+
+}
