6ab04993f880e37e0bd53ebdd28045508f2593b2
Bernhard Posselt <dev@bernhard-posselt.com>
2014-11-06 09:46:22 +0100

    first half working integration test

--- tests/integration/db/ItemMapperTest.php
+++ tests/integration/db/ItemMapperTest.php
@@ -15,10 +15,49 @@ class ItemMapperTest extends NewsIntegrationTest {
         $app = new Application();
         $this->container = $app->getContainer();
         $this->itemMapper = $this->container->query('ItemMapper');
+        $this->feedMapper = $this->container->query('FeedMapper');
+        $this->folderMapper = $this->container->query('FolderMapper');
+    }
+
+
+    private function setupFeedAndFolder($feedOptions=[], $folderOptions=[]) {
+        $folderDefault = [
+            'id' => 5,
+            'userId' => $this->userId,
+            'name' => 'a folder',
+            'parentId' => 0
+        ];
+        $feedDefault = [
+            'id' => 3,
+            'userId' => $this->userId,
+            'url' => 'http://google.com',
+            'title' => 'le feed',
+            'folderId' => 5
+        ];
+
+        $folderDefault = array_merge($folderDefault, $folderOptions);
+        $feedDefault = array_merge($feedDefault, $feedOptions);
+
+        $feed = new Feed();
+        foreach ($feedDefault as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            $feed->$method($value);
+        }
+        $this->feedMapper->insert($feed);
+
+        $folder = new Folder();
+        foreach ($folderDefault as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            $folder->$method($value);
+        }
+        $this->folderMapper->insert($folder);
+
     }
 
 
     public function testInsert() {
+        $this->setupFeedAndFolder();
+
         $item = new Item();
         $item->setTitle('my title');
         $item->setGuid('test');
@@ -27,10 +66,7 @@ class ItemMapperTest extends NewsIntegrationTest {
 
         $created = $this->itemMapper->insert($item);
 
-        var_dump($created);
-
         $fetched = $this->itemMapper->find($created->getId(), $this->userId);
-
 
         $this->assertEquals($item->getTitle(), $fetched->getTitle());
         $this->assertEquals($item->getGuid(), $fetched->getGuid());
