1aa9d9a70daf75995822aa7170f11504cd6931bb
Bernhard Posselt <dev@bernhard-posselt.com>
2014-11-23 20:09:29 +0100

    fix #653

--- appinfo/application.php
+++ appinfo/application.php
@@ -427,7 +427,9 @@ class Application extends App {
                 ->setClientTimeout($config->getFeedFetcherTimeout())
                 ->setMaxRedirections($config->getMaxRedirects())
                 ->setMaxBodySize($config->getMaxSize())
-                ->setContentFiltering(false)
+                // enable again if we can distinguish between security and
+                // content filtering
+                //->setContentFiltering(false)
                 ->setParserHashAlgo('md5');
 
             // proxy settings
