368697b24bd28fdbead03435fe3656f1fc337836
Bernhard Posselt <dev@bernhard-posselt.com>
2015-01-25 18:38:56 +0100

    more plugin refactoring

--- js/directive/NewsArticleActions.js
+++ js/directive/NewsArticleActions.js
@@ -11,18 +11,14 @@ app.directive('newsArticleActions', function () {
     'use strict';
     return {
         restrict: 'E',
-        templateUrl: 'articleaction.html',
         scope: {
             'article': '='
         },
-        replace: true,
         link: function (scope, elem) {
             var plugins = News.getArticleActionPlugins();
-            scope.plugins = [];
 
             for (var i=0; i<plugins.length; i+=1) {
-                var plugin = new plugins[i](elem, scope.article);
-                scope.plugins.push(plugin);
+                plugins[i](elem, scope.article);
             }
         }
     };
--- templates/part.content.articleaction.php
+++
@@ -1,6 +0,0 @@
-<ul class="article-action-plugins" news-stop-propagation>
-    <li ng-repeat="plugin in ::plugins"
-        class="util article-plugin-{{ plugin.id }}">
-        <button title="{{ plugin.title }}"></button>
-    </li>
-</ul>
