19719fea227a454e742b3258d3565ad0ce04fd23
Bernhard Posselt <dev@bernhard-posselt.com>
2015-08-07 15:43:48 +0200

    fix line issue

--- appinfo/register_command.php
+++ appinfo/register_command.php
@@ -9,8 +9,10 @@
  * @copyright Bernhard Posselt 2015
  */
 
+use OCA\News\Command\GenerateSearchIndices;
+
 $newsApp = new OCA\News\AppInfo\Application();
 $newsContainer = $newsApp->getContainer();
-$newsCmd = $newsContainer->query(\OCA\News\Command\GenerateSearchIndices::class);
+$newsCmd = $newsContainer->query(GenerateSearchIndices::class);
 
 $application->add($newsCmd);
