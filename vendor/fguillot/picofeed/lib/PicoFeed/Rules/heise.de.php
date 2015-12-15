ba21544ab6dcc7949895acba93cb7c392a941031
Bernhard Posselt <dev@bernhard-posselt.com>
2015-12-15 20:00:33 +0100

    update picofeed

--- vendor/fguillot/picofeed/lib/PicoFeed/Rules/heise.de.php
+++ vendor/fguillot/picofeed/lib/PicoFeed/Rules/heise.de.php
@@ -6,6 +6,7 @@ return array(
             'test_url' => 'http://www.heise.de/security/meldung/BND-300-Millionen-Euro-fuer-Fruehwarnsystem-gegen-Cyber-Attacken-2192237.html',
             'body' => array(
                 '//div[@class="meldung_wrapper"]',
+                '//div[@class="artikel_content"]',
             ),
         ),
     ),
