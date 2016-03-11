984d1d963ee4feb011f077d8ab6bb0b56d882969
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-03-11 01:01:44 -0500

    [tx-robot] updated from transifex

--- l10n/uk.js
+++ l10n/uk.js
@@ -37,6 +37,7 @@ OC.L10N.register(
     "Close" : "Закрити",
     "How to set up the operating system cron" : "Як налаштувати cron операційної системи",
     "How to set up a custom updater (faster and no possible deadlock) " : "Як налаштувати інший спосіб оновлення (швидше і без ризику взаємного блокування)",
+    "filter" : "фільтр",
     "Language" : "Мова",
     "Subscribe" : "Підписатися",
     "Refresh" : "Оновити",
