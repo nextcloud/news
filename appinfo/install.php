51095a9276fe24cdc2127d85ca461338e27e8ba3
Bernhard Posselt <dev@bernhard-posselt.com>
2017-05-24 20:33:45 +0200

    better link

--- appinfo/install.php
+++ appinfo/install.php
@@ -13,6 +13,6 @@ if ($platform instanceof MySqlPlatform && $charset !== 'utf8mb4') {
     $msg = 'App can not be installed because database MySql/MariaDb uses a ' .
            'non UTF8 charset: ' . $charset .'. Learn more on how to migrate ' .
            'your database to utf8mb4 after making a backup at ' .
-           'https://dba.stackexchange.com/questions/8239/how-to-easily-convert-utf8-tables-to-utf8mb4-in-mysql-5-5';
+           'https://dba.stackexchange.com/a/21684';
     throw new Exception($msg);
 }
