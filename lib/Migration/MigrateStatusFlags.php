193c926ef66792269590d972e3a6403a8b7a5ea9
Daniel Opitz <git@copynpaste.de>
2017-07-13 00:29:13 +0200

    add repair step for migration

---
+++ lib/Migration/MigrateStatusFlags.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Nextcloud - News
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Daniel Opitz <dev@copynpaste.de>
+ * @copyright Daniel Opitz 2017
+ */
+
+namespace OCA\News\Migration;
+
+use OCP\IConfig;
+use OCP\IDBConnection;
+use OCP\Migration\IRepairStep;
+use OCP\Migration\IOutput;
+
+class MigrateStatusFlags implements IRepairStep {
+
+    /** @var IDBConnection */
+    private $db;
+
+    /** @var IConfig */
+    private $config;
+
+    /**
+     * @param IDBConnection $db
+     * @param IConfig $config
+     */
+    public function __construct(IDBConnection $db, IConfig $config) {
+        $this->db = $db;
+        $this->config = $config;
+    }
+
+    public function getName() {
+        return 'Migrate binary status into separate boolean fields';
+    }
+
+    public function run(IOutput $output) {
+        $version = $this->config->getAppValue('news', 'installed_version', '0.0.0');
+        if (version_compare($version, '11.0.6', '>=')) {
+            return;
+        }
+
+        $update = 'UPDATE `*PREFIX*news_items` ' .
+            'SET unread = IF(status & 2, true, false), starred = IF(status & 4, true, false)';
+
+        $output->startProgress();
+        $this->db->executeUpdate($update);
+        $output->finishProgress();
+    }
+
+}
