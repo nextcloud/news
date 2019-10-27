e1c691ba1eafd2a40edbc7e7fc1a81866e68c226
b_b <bruno@eliaz.fr>
2019-10-27 10:34:37 +0100

    Generate enclosure div only for audio & video
    Fix #532
    
    Signed-off-by: brunob <bruno@eliaz.fr>

--- templates/part.content.php
+++ templates/part.content.php
@@ -130,7 +130,7 @@
                     </span>
                 </div>
 
-                <div class="enclosure" ng-if="item.enclosureLink">
+                <div class="enclosure" ng-if="Content.getMediaType(item.enclosureMime) == 'audio' || Content.getMediaType(item.enclosureMime) == 'video'">
                     <video controls preload="none" ng-if="Content.getMediaType(item.enclosureMime) =='video'" news-play-one ng-src="{{ item.enclosureLink|trustUrl }}" type="{{ item.enclosureMime }}">
                     </video>
                     <button ng-if="Content.getMediaType(item.enclosureMime) == 'audio'" ng-click="App.play(item)"><?php p($l->t('Play audio')) ?></button>
