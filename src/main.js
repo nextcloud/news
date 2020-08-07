5d597f51024f38553d7e417244884fc7007d71ff
Jonathan Treffler <mail@jonathan-treffler.de>
2020-08-07 12:27:39 +0200

    added Vuex to main.js
    Signed-off-by: Jonathan Treffler <mail@jonathan-treffler.de>

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 
 Vue.prototype.t = t
 Vue.prototype.n = n
 Vue.prototype.OC = OC
 Vue.prototype.OCA = OCA
+
+Vue.use(Vuex)
 
 export default new Vue({
 	el: '#content',
