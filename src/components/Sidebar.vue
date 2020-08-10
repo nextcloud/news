48e759f485732ab26669b7a5532bc69123fe3e34
Jonathan Treffler <mail@jonathan-treffler.de>
2020-08-10 15:59:26 +0200

    added Sidebar component
    Signed-off-by: Jonathan Treffler <mail@jonathan-treffler.de>

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,38 @@
+<template>
+	<AppNavigation>
+		<AppNavigationNew
+			:text="t('news','Subscribe')"
+			button-id="new-feed-button"
+			button-class="icon-add" />
+		<ul id="locations" class="with-icon">
+			<AppNavigationItem :title="t('news','Unread articles')" icon="icon-rss">
+				<AppNavigationCounter slot="counter">
+					5
+				</AppNavigationCounter>
+			</AppNavigationItem>
+			<AppNavigationItem :title="t('news','New folder')" icon="icon-add" />
+		</ul>
+	</AppNavigation>
+</template>
+
+<script>
+import AppNavigation from '@nextcloud/vue/dist/Components/AppNavigation'
+import AppNavigationNew from '@nextcloud/vue/dist/Components/AppNavigationNew'
+import AppNavigationItem from '@nextcloud/vue/dist/Components/AppNavigationItem'
+import AppNavigationCounter from '@nextcloud/vue/dist/Components/AppNavigationCounter'
+
+export default {
+	components: {
+		AppNavigation,
+		AppNavigationNew,
+		AppNavigationItem,
+		AppNavigationCounter,
+	},
+}
+</script>
+
+<style>
+.icon-rss {
+	background-image: url(/img/rss.svg) !important;
+}
+</style>
