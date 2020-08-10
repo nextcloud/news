1a9c3bfeb722447fdddfad02ab67c492ad25d634
Jonathan Treffler <mail@jonathan-treffler.de>
2020-08-10 15:55:11 +0200

    moved image loading to file-loader
    Signed-off-by: Jonathan Treffler <mail@jonathan-treffler.de>

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
-	<div id="content" class="app-news" />
+	<Content app-name="news">
+		<Sidebar />
+	</Content>
 </template>
+
+<script>
+import Content from '@nextcloud/vue/dist/Components/Content'
+import Sidebar from './components/Sidebar.vue'
+
+export default {
+	components: {
+		Content,
+		Sidebar,
+	},
+}
+</script>
