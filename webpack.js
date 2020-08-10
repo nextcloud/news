dcbdde951b26ec54f65aea6c0eda667dfcb7d055
Jonathan Treffler <mail@jonathan-treffler.de>
2020-08-10 15:25:09 +0200

    imporved webpack config, to support images
    Signed-off-by: Jonathan Treffler <mail@jonathan-treffler.de>

--- webpack.js
+++ webpack.js
@@ -1,3 +1,18 @@
+const { merge } = require('webpack-merge')
+const webpack = require('webpack')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
-module.exports = webpackConfig
+const config = {
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|png|svg)$/,
+                use: {
+                    loader: 'url-loader',
+                },
+            }
+        ],
+    },
+}
+
+module.exports = merge(config, webpackConfig)
