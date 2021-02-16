4d4fca730f40d610927a97e957158dc6ae5f7169
WENDLING NICOLAS <nicolas.wendling2@etu.unistra.fr>
2021-02-16 16:16:41 +0100

    ✅ Unary test for itemShare from itemService class
    Signed-off-by: Marco Nassabain <marco.nassabain@hotmail.com>

--- tests/Unit/Service/ItemServiceTest.php
+++ tests/Unit/Service/ItemServiceTest.php
@@ -630,4 +630,29 @@ class ItemServiceTest extends TestCase
         $this->class->purgeOverThreshold(5);
     }
 
+    public function testShareItem()
+    {
+        $itemId = 3;
+
+        $this->mapper->expects($this->once())
+            ->method('shareItem')
+            ->with(
+                $this->equalTo($itemId),
+                $this->equalTo('john'),
+                $this->equalTo('jack')
+            )
+            ->will($this->returnValue(new Item()));
+
+        $this->itemService->shareItem($itemId, 'john','jack');
+    }
+
+    public function testShareItemDoesNotExist()
+    {
+        $this->expectException(ServiceNotFoundException::class);
+        $this->mapper->expects($this->once())
+            ->method('shareItem')
+            ->will($this->throwException(new DoesNotExistException('')));
+
+        $this->itemService->shareItem(1, 'john', 'jack');
+    }
 }
