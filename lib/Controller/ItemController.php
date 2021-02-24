241b266ce88ff92e7a1f532e0d173ad8f0a7adb0
Marco Nassabain <marco.nassabain@hotmail.com>
2021-02-24 23:07:17 +0100

    ✨ ItemController (index): add case for shared items
    Signed-off-by: Marco Nassabain <marco.nassabain@hotmail.com>

--- lib/Controller/ItemController.php
+++ lib/Controller/ItemController.php
@@ -162,6 +162,17 @@ class ItemController extends Controller
                         $search_items
                     );
                     break;
+                case ListType::SHARED:
+                    $items = $this->itemService->findAllSharedWithUserWithFilters(
+                        $this->getUserId(),
+                        $limit,
+                        $offset,
+                        !$showAll,
+                        $oldestFirst,
+                        $search_items
+                    );
+                    break;
+
                 default:
                     $items = $this->itemService->findAllWithFilters(
                         $this->getUserId(),
