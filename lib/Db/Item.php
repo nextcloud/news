982332272b107a0b3c8cb9958ef4bb290d24cf95
Marco Nassabain <marco.nassabain@hotmail.com>
2021-03-03 23:26:06 +0100

    🎨 Item.php: make shared_by nullable
    Signed-off-by: Marco Nassabain <marco.nassabain@hotmail.com>

--- lib/Db/Item.php
+++ lib/Db/Item.php
@@ -68,7 +68,7 @@ class Item extends Entity implements IAPI, \JsonSerializable
     /** @var string|null */
     protected $categoriesJson;
     /** @var string */
-    protected $sharedBy = '';
+    protected $sharedBy;
 
 
     public function __construct()
@@ -297,7 +297,7 @@ class Item extends Entity implements IAPI, \JsonSerializable
         return $this->unread;
     }
 
-    public function getSharedBy(): string
+    public function getSharedBy(): ?string
     {
         return $this->sharedBy;
     }
@@ -528,7 +528,7 @@ class Item extends Entity implements IAPI, \JsonSerializable
         return $this;
     }
 
-    public function setSharedBy(string $sharedBy): self
+    public function setSharedBy(string $sharedBy = null): self
     {
         if ($this->sharedBy !== $sharedBy) {
             $this->sharedBy = $sharedBy;
