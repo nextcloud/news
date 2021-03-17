1351b6b1f72a5f6d693299a92c2e871933f51e51
Tucker McKnight <tucker.mcknight@gmail.com>
2021-03-16 23:51:00 -0600

    Put some nextcloud vue components on the page

--- js/vue_components/component.vue
+++ js/vue_components/component.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <p>asdf newest first is {{this.oldestFirst.toString()}}</p>
+    <Actions>
+      <ActionButton icon="icon-delete">Delete something</ActionButton>
+      <ActionButton icon="icon-add">Add Something</ActionButton>
+    </Actions>
   </div>
 </template>
 
 <script>
-// import { ActionButton } from '@nextcloud/vue/dist/Components';
+import Actions from '@nextcloud/vue/dist/Components/Actions';
+import ActionButton from '@nextcloud/vue/dist/Components/ActionButton';
 
-module.exports = {
+export default {
   props: {
     oldestFirst: Boolean
+  },
+  components: {
+    Actions,
+    ActionButton,
   }
 };
 </script>
