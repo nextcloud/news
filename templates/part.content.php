3494e602903e4cd0e25e626b76b51be4e95a8a05
Marco Nassabain <marco.nassabain@hotmail.com>
2021-03-19 00:00:11 +0100

    ✨ Shared items: show sharer display name
    Signed-off-by: Marco Nassabain <marco.nassabain@hotmail.com>

--- templates/part.content.php
+++ templates/part.content.php
@@ -167,7 +167,7 @@
                             </div>
                             <div ng-if="Share.isSocialAppEnabled('email')" class="col-4">
                                 <a class="icon-dropdown icon-mail pr-5"
-                                    ng-href="{{ Share.getEmailUrl(item.url, '<?php p($l->t('I wanted you to see this article')) ?>', '<?php p($l->t('Check out this article')) ?>') }}"></a>    
+                                    ng-href="{{ Share.getEmailUrl(item.url, '<?php p($l->t('I wanted you to see this article')) ?>', '<?php p($l->t('Check out this article')) ?>') }}"></a>
                             </div>
                         </div>
                     </div>
@@ -209,7 +209,7 @@
                     <span ng-if="item.sharedBy">
                         <span ng-if="item.author">-</span>
                         <?php p($l->t('shared by')) ?>
-                        {{ ::item.sharedBy }}
+                        {{ ::item.sharedByDisplayName }}
                     </span>
                 </div>
 
