35dc4cf033ee828759481f50952a4376f6d6c656
Marco Nassabain <marco.nassabain@hotmail.com>
2021-03-18 23:00:30 +0100

    🗃 Update share migration version
    Signed-off-by: Marco Nassabain <marco.nassabain@hotmail.com>

---
+++ lib/Migration/Version150400Date20210318215425.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\News\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version150400Date20210318215425 extends SimpleMigrationStep {
+
+    /**
+     * @param IOutput $output
+     * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+     * @param array $options
+     */
+    public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+    }
+
+    /**
+     * @param IOutput $output
+     * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+     * @param array $options
+     * @return null|ISchemaWrapper
+     */
+    public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+        /** @var ISchemaWrapper $schema */
+        $schema = $schemaClosure();
+
+        if ($schema->hasTable('news_items')) {
+            $table = $schema->getTable('news_items');
+            $table->addColumn('shared_by', 'string', [
+                'notnull' => false,
+                'length' => 64
+            ]);
+        }
+
+        return $schema;
+    }
+
+    /**
+     * @param IOutput $output
+     * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+     * @param array $options
+     */
+    public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+    }
+}
