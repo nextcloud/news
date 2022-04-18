174151e425fe847056c235ad80be64040adeac94
nextcloud486153 <78801830+nextcloud486153@users.noreply.github.com>
2022-04-18 14:12:03 +0200

    Update test url (pending from #1739)
    Signed-off-by: nextcloud486153 <78801830+nextcloud486153@users.noreply.github.com>

--- tests/integration/helpers/settings.bash
+++ tests/integration/helpers/settings.bash
@@ -1,4 +1,4 @@
 user=admin
 NC_FEED="https://nextcloud.com/blog/static-feed/"
 HEISE_FEED="https://www.heise.de/rss/heise-atom.xml"
-NO_GUID_FEED="https://raw.githubusercontent.com/nextcloud486153/nextcloud-app-news/blob/master/tests/integration/feeds/no_guid_feed.xml"
+NO_GUID_FEED="https://raw.githubusercontent.com/nextcloud/news/blob/master/tests/integration/feeds/no_guid_feed.xml"
