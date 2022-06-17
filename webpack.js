ef1e6053540f408fc46a1c24efc487a6a15262b9
Devlin Junker <devlin.junker@gmail.com>
2022-06-17 09:25:05 -0700

    update changelog and fix comment
    Signed-off-by: Devlin Junker <devlin.junker@gmail.com>

--- webpack.js
+++ webpack.js
@@ -3,7 +3,7 @@ const webpackConfig = require('@nextcloud/webpack-vue-config')
 // set mode to production if `npm run build` called
 webpackConfig.mode = process.env.NODE_ENV === 'production' ? 'production' : 'development'
 
-// Add Babel Loader before TS Loader to process typescript (babel is needed for decorators for some reason?)
+// Add Babel Loader before TS Loader for processing typescript in vue templates (babel is needed for decorators for some reason?)
 webpackConfig.module.rules.push({
     test: /.ts$/,
     exclude: [/node_modules/],
