264758b131fee97bcc3e2d1073a816a1653579d1
Devlin Junker <devlin.junker@gmail.com>
2022-06-22 21:52:56 -0700

    clean up babel-loader
    Signed-off-by: Devlin Junker <devlin.junker@gmail.com>

--- webpack.js
+++ webpack.js
@@ -1,11 +1,10 @@
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
-// Add Babel Loader before TS Loader for processing typescript in vue templates (babel is needed for decorators for some reason?)
+// Add TS Loader for processing typescript in vue templates
 webpackConfig.module.rules.push({
     test: /.ts$/,
     exclude: [/node_modules/],
     use: [
-        'babel-loader',
         {
             loader: 'ts-loader',
             options: {
