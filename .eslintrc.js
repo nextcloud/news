42e9b47159cb95a9152e7ea66fe26abce5beeb9e
Devlin Junker <devlin.junker@gmail.com>
2022-06-30 21:28:56 -0700

    change linting rule from spaces to tabs to match typical vue linting
    also clean up all lint warnings in js+vue files
    
    Signed-off-by: Devlin Junker <devlin.junker@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,22 @@
 module.exports = {
-    root: true,
-    parser: 'vue-eslint-parser',
-    parserOptions: {
-        parser: {
-            ts: '@typescript-eslint/parser',
-        },
-        ecmaVersion: 2020,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:vue/base',
-        'plugin:vue/essential',
-        '@vue/standard',
-        '@vue/typescript/recommended',
-        '@nextcloud',
-    ],
-    ignorePatterns: ['*.d.ts'],
-    rules: {
-        'node/no-unpublished-import': 'off', // necessary for vue-property-decorator (not published?)
-
-        // TODO: remove these indentation rules during reformat (expects tab char \t but right now code base uses spaces)
-        'vue/html-indent': 'off',
-        indent: ['error', 4],
-    },
+	root: true,
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		parser: {
+			ts: '@typescript-eslint/parser',
+		},
+		ecmaVersion: 2020,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:vue/base',
+		'plugin:vue/essential',
+		'@vue/standard',
+		'@vue/typescript/recommended',
+		'@nextcloud',
+	],
+	ignorePatterns: ['*.d.ts'],
+	rules: {
+		'node/no-unpublished-import': 'off', // necessary for vue-property-decorator (not published?)
+	},
 }
--- src/components/AddFeed.vue
+++ src/components/AddFeed.vue
@@ -1,45 +1,45 @@
 <template>
-    <Modal @close="$emit('close')">
-        <div id="new-feed" news-add-feed="Navigation.feed">
-            <form ng-submit="Navigation.createFeed(Navigation.feed)"
-                ng-init="Navigation.feed.autoDiscover=true"
-                name="feedform">
-                <fieldset ng-disabled="Navigation.addingFeed" style="padding: 16px">
-                    <input type="text"
-                        :value="feed"
-                        ng-model="Navigation.feed.url"
-                        ng-class="{'ng-invalid':
+	<Modal @close="$emit('close')">
+		<div id="new-feed" news-add-feed="Navigation.feed">
+			<form ng-submit="Navigation.createFeed(Navigation.feed)"
+				ng-init="Navigation.feed.autoDiscover=true"
+				name="feedform">
+				<fieldset ng-disabled="Navigation.addingFeed" style="padding: 16px">
+					<input type="text"
+						:value="feed"
+						ng-model="Navigation.feed.url"
+						ng-class="{'ng-invalid':
                         !Navigation.addingFeed &&
                         Navigation.feedUrlExists(Navigation.feed.url)
                     }"
-                        :placeholder="t('news', 'Web address')"
-                        name="address"
-                        pattern="[^\s]+"
-                        required
-                        autofocus />
+						:placeholder="t('news', 'Web address')"
+						name="address"
+						pattern="[^\s]+"
+						required
+						autofocus>
 
-                    <p class="error"
-                        ng-show="!Navigation.addingFeed &&
+					<p class="error"
+						ng-show="!Navigation.addingFeed &&
                         Navigation.feedUrlExists(Navigation.feed.url)">
-                        {{ t("news", "Feed exists already!") }}
-                    </p>
+						{{ t("news", "Feed exists already!") }}
+					</p>
 
-                    <!-- select a folder -->
-                    <CheckboxRadioSwitch :checked.sync="createNewFolder" type="switch">
-                        {{ t("news", "New folder") }}?
-                    </CheckboxRadioSwitch>
+					<!-- select a folder -->
+					<CheckboxRadioSwitch :checked.sync="createNewFolder" type="switch">
+						{{ t("news", "New folder") }}?
+					</CheckboxRadioSwitch>
 
-                    <Multiselect v-if="!createNewFolder"
-                        v-model="folder"
-                        :options="folders"
-                        track-by="id"
-                        label="name" />
+					<Multiselect v-if="!createNewFolder"
+						v-model="folder"
+						:options="folders"
+						track-by="id"
+						label="name" />
 
-                    <!-- add a folder -->
-                    <input v-if="createNewFolder"
-                        type="text"
-                        ng-model="Navigation.feed.newFolder"
-                        ng-class="{'ng-invalid':
+					<!-- add a folder -->
+					<input v-if="createNewFolder"
+						type="text"
+						ng-model="Navigation.feed.newFolder"
+						ng-class="{'ng-invalid':
                             !Navigation.addingFeed &&
                             !Navigation.addingFeed &&
                             Navigation.showNewFolder &&
@@ -47,64 +47,64 @@
                                 Navigation.feed.newFolder
                             )
                         }"
-                        :placeholder="t('news', 'Folder name')"
-                        name="folderName"
-                        style="width: 90%"
-                        required />
+						:placeholder="t('news', 'Folder name')"
+						name="folderName"
+						style="width: 90%"
+						required>
 
-                    <p class="error"
-                        ng-show="!Navigation.addingFeed &&
+					<p class="error"
+						ng-show="!Navigation.addingFeed &&
                     Navigation.folderNameExists(Navigation.feed.newFolder)">
-                        {{ t("news", "Folder exists already!") }}
-                    </p>
+						{{ t("news", "Folder exists already!") }}
+					</p>
 
-                    <!-- basic auth -->
+					<!-- basic auth -->
 
-                    <CheckboxRadioSwitch :checked.sync="withBasicAuth" type="switch">
-                        {{ t("news", "Credentials") }}?
-                    </CheckboxRadioSwitch>
+					<CheckboxRadioSwitch :checked.sync="withBasicAuth" type="switch">
+						{{ t("news", "Credentials") }}?
+					</CheckboxRadioSwitch>
 
-                    <div v-if="withBasicAuth" class="add-feed-basicauth">
-                        <p class="warning">
-                            {{
-                                t(
-                                    "news",
-                                    "HTTP Basic Auth credentials must be stored unencrypted! Everyone with access to the server or database will be able to access them!"
-                                )
-                            }}>
-                        </p>
-                        <input type="text"
-                            ng-model="Navigation.feed.user"
-                            :placeholder="t('news', 'Username')"
-                            name="user"
-                            autofocus />
+					<div v-if="withBasicAuth" class="add-feed-basicauth">
+						<p class="warning">
+							{{
+								t(
+									"news",
+									"HTTP Basic Auth credentials must be stored unencrypted! Everyone with access to the server or database will be able to access them!"
+								)
+							}}>
+						</p>
+						<input type="text"
+							ng-model="Navigation.feed.user"
+							:placeholder="t('news', 'Username')"
+							name="user"
+							autofocus>
 
-                        <input type="password"
-                            ng-model="Navigation.feed.password"
-                            :placeholder="t('news', 'Password')"
-                            name="password"
-                            autocomplete="new-password" />
-                    </div>
+						<input type="password"
+							ng-model="Navigation.feed.password"
+							:placeholder="t('news', 'Password')"
+							name="password"
+							autocomplete="new-password">
+					</div>
 
-                    <CheckboxRadioSwitch :checked.sync="autoDiscover" type="switch">
-                        {{ t("news", "Auto discover Feed") }}?
-                    </CheckboxRadioSwitch>
+					<CheckboxRadioSwitch :checked.sync="autoDiscover" type="switch">
+						{{ t("news", "Auto discover Feed") }}?
+					</CheckboxRadioSwitch>
 
-                    <Button :wide="true"
-                        type="primary"
-                        ng-disabled="
+					<Button :wide="true"
+						type="primary"
+						ng-disabled="
                         Navigation.feedUrlExists(Navigation.feed.url) ||
                                 (
                                     Navigation.showNewFolder &&
                                     Navigation.folderNameExists(folder.name)
                                 )"
-                        @click="addFeed()">
-                        {{ t("news", "Subscribe") }}
-                    </Button>
-                </fieldset>
-            </form>
-        </div>
-    </Modal>
+						@click="addFeed()">
+						{{ t("news", "Subscribe") }}
+					</Button>
+				</fieldset>
+			</form>
+		</div>
+	</Modal>
 </template>
 
 <script>
@@ -116,46 +116,49 @@ import Button from '@nextcloud/vue/dist/Components/Button'
 import Multiselect from '@nextcloud/vue/dist/Components/Multiselect'
 
 export default {
-    components: {
-        Modal,
-        CheckboxRadioSwitch,
-        Button,
-        Multiselect,
-    },
-    props: {
-        feed: '',
-    },
-    emits: ['close'],
-    data() {
-        return {
-            folder: {},
-            autoDiscover: true,
-            createNewFolder: false,
-            withBasicAuth: false,
-        }
-    },
-    computed: {
-        folders() {
-            return this.$store.state.folders
-        },
-    },
-    methods: {
-        newFolder() {
-            this.createNewFolder = true
-        },
-        abortNewFolder() {
-            this.createNewFolder = false
-        },
-        addFeed() {
-            this.$store.dispatch('addFeed', {
-                feedReq: {
-                    url: this.feed,
-                    folder: this.folder,
-                    autoDiscover: true,
-                },
-            })
-        },
-    },
+	components: {
+		Modal,
+		CheckboxRadioSwitch,
+		Button,
+		Multiselect,
+	},
+	props: {
+		feed: {
+			type: String,
+			default: '',
+		},
+	},
+	emits: ['close'],
+	data() {
+		return {
+			folder: {},
+			autoDiscover: true,
+			createNewFolder: false,
+			withBasicAuth: false,
+		}
+	},
+	computed: {
+		folders() {
+			return this.$store.state.folders
+		},
+	},
+	methods: {
+		newFolder() {
+			this.createNewFolder = true
+		},
+		abortNewFolder() {
+			this.createNewFolder = false
+		},
+		addFeed() {
+			this.$store.dispatch('addFeed', {
+				feedReq: {
+					url: this.feed,
+					folder: this.folder,
+					autoDiscover: true,
+				},
+			})
+		},
+	},
 }
 </script>
 
