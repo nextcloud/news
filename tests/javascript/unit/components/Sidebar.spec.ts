b1ce62801cc63e54de4bd247cc5045420cdb8efa
Devlin Junker <devlin.junker@gmail.com>
2022-10-02 20:58:55 -0700

    outline Sidebar specs
    Signed-off-by: Devlin Junker <devlin.junker@gmail.com>

--- tests/javascript/unit/components/Sidebar.spec.ts
+++ tests/javascript/unit/components/Sidebar.spec.ts
@@ -1,24 +1,3 @@
-/**
- * Nextcloud - Tasks
- *
- * @author Raimund Schlüßler
- *
- * @copyright 2021 Raimund Schlüßler <raimund.schluessler@mailbox.org>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
- *
- * You should have received a copy of the GNU Affero General Public
- * License along with this library. If not, see <http://www.gnu.org/licenses/>.
- *
- */
 import AppSidebar from 'Components/Sidebar.vue'
 
 import { store, localVue } from '../setupStore'
@@ -33,4 +12,12 @@ describe('Sidebar.vue', () => {
 
 		expect(wrapper.vm.$data.showAddFeed).toBeFalsy
 	});
+
+	it('should dispatch message to store with folder name to create new folder');
+
+	it('should dispatch message to store with folder object on delete folder')
+
+	it('should set showAddFeed to true')
+
+	it('should set showAddFeed to false')
 })
