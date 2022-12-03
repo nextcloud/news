c208c01b8576123652d4635e2c6dbc7bed6cd291
Devlin Junker <devlin.junker@gmail.com>
2022-12-03 14:34:26 -0800

    add more tests
    Signed-off-by: Devlin Junker <devlin.junker@gmail.com>

--- tests/javascript/unit/components/Sidebar.spec.ts
+++ tests/javascript/unit/components/Sidebar.spec.ts
@@ -29,41 +29,104 @@ describe('Sidebar.vue', () => {
 		expect((wrapper.vm as any).$data.showAddFeed).toBeFalsy()
 	})
 
-	it('should dispatch message to store with folder name to create new folder', () => {
-		(wrapper.vm as any).newFolder('abc')
+	describe('User Actions', () => {
+		it('should dispatch message to store with folder name to create new folder', () => {
+			(wrapper.vm as any).newFolder('abc')
 
-		expect((wrapper.vm as any).$store.dispatch).toHaveBeenCalledWith(ACTIONS.ADD_FOLDERS, { folder: { name: 'abc' } })
-	})
-
-	it('should dispatch message to store with folder object on delete folder', () => {
-		const folder = {};
-		(wrapper.vm as any).deleteFolder(folder)
-
-		expect((wrapper.vm as any).$store.dispatch).toHaveBeenCalledWith(ACTIONS.DELETE_FOLDER, { folder })
-	})
-
-	it('should set showAddFeed to true', () => {
-		(wrapper.vm as any).showShowAddFeed()
-		expect(wrapper.vm.$data.showAddFeed).toBeTruthy()
-	})
-
-	it('should set showAddFeed to false', () => {
-		(wrapper.vm as any).closeShowAddFeed()
-		expect(wrapper.vm.$data.showAddFeed).toBeFalsy()
-	})
-
-	// TODO: A couple more tests here
-	it('should return top level nav (folders and feeds without folders)', () => {
-		const topLevelNav = (wrapper.vm.$options.computed?.topLevelNav as any).call({
-			$store: {
-				getters: {
-					feeds: [],
-					folders: [],
-				},
-			},
+			expect((wrapper.vm as any).$store.dispatch).toHaveBeenCalledWith(ACTIONS.ADD_FOLDERS, { folder: { name: 'abc' } })
 		})
 
-		expect(topLevelNav).toEqual([])
+		it('should dispatch message to store with folder object on delete folder', () => {
+			const folder = {};
+			(wrapper.vm as any).deleteFolder(folder)
+
+			expect((wrapper.vm as any).$store.dispatch).toHaveBeenCalledWith(ACTIONS.DELETE_FOLDER, { folder })
+		})
+
+		it('should set showAddFeed to true', () => {
+			(wrapper.vm as any).showShowAddFeed()
+			expect(wrapper.vm.$data.showAddFeed).toBeTruthy()
+		})
+
+		it('should set showAddFeed to false', () => {
+			(wrapper.vm as any).closeShowAddFeed()
+			expect(wrapper.vm.$data.showAddFeed).toBeFalsy()
+		})
+	})
+
+	describe('SideBarState', () => {
+		it('should return no top level nav when no folders or feeds', () => {
+			const topLevelNav = (wrapper.vm.$options.computed?.topLevelNav as any).call({
+				$store: {
+					getters: {
+						feeds: [],
+						folders: [],
+					},
+				},
+			})
+
+			expect(topLevelNav).toEqual([])
+		})
+
+		it('should return top level nav with 1 feed', () => {
+			const feeds: any[] = [{ name: 'feed1', id: 1 }]
+			const folders: any[] = []
+			const topLevelNav = (wrapper.vm.$options.computed?.topLevelNav as any).call({
+				$store: {
+					getters: {
+						feeds,
+						folders,
+					},
+				},
+			})
+
+			expect(topLevelNav).toEqual([feeds[0]])
+		})
+
+		it('should return top level nav with 1 folder (with feeds)', () => {
+			const feeds: any[] = [{ name: 'feed2', id: 2, folderId: 123 }]
+			const folders: any[] = [{ name: 'abc', id: 123 }]
+			const topLevelNav = (wrapper.vm.$options.computed?.topLevelNav as any).call({
+				$store: {
+					getters: {
+						feeds,
+						folders,
+					},
+				},
+			})
+
+			expect(topLevelNav).toEqual(folders)
+		})
+
+		it('should return top level nav with 1 folder (without feed)', () => {
+			const feeds: any[] = [{ name: 'feed1', id: 1 }]
+			const folders: any[] = [{ name: 'abc', id: 123 }]
+			const topLevelNav = (wrapper.vm.$options.computed?.topLevelNav as any).call({
+				$store: {
+					getters: {
+						feeds,
+						folders,
+					},
+				},
+			})
+
+			expect(topLevelNav).toEqual([feeds[0], ...folders])
+		})
+
+		it('should return top level nav with feeds and folders', () => {
+			const feeds: any[] = [{ name: 'feed1', id: 1 }, { name: 'feed2', id: 2, folderId: 123 }]
+			const folders: any[] = [{ name: 'abc', id: 123 }, { name: 'xyz', id: 234 }]
+			const topLevelNav = (wrapper.vm.$options.computed?.topLevelNav as any).call({
+				$store: {
+					getters: {
+						feeds,
+						folders,
+					},
+				},
+			})
+
+			expect(topLevelNav).toEqual([feeds[0], ...folders])
+		})
 	})
 
 	// TODO: More Template Testing with https://test-utils.vuejs.org/guide/essentials/a-crash-course.html#adding-a-new-todo
@@ -72,7 +135,7 @@ describe('Sidebar.vue', () => {
 		jest.clearAllMocks()
 	})
 
-	describe('SideBar State', () => {
-		// TODO
+	afterEach(() => {
+		jest.clearAllMocks()
 	})
 })
