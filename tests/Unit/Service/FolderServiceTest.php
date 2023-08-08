9a047cbbcdfb17ebf9b46aee8eeb17ae7f17a160
Devlin Junker <devlin.junker@gmail.com>
2023-08-08 16:09:08 -0700

    minimum nextcloud version supported is 27
    Signed-off-by: Devlin Junker <devlin.junker@gmail.com>

--- tests/Unit/Service/FolderServiceTest.php
+++ tests/Unit/Service/FolderServiceTest.php
@@ -69,19 +69,6 @@ class FolderServiceTest extends TestCase
         $timeFactoryBuilder = $this->getMockBuilder(TimeFactory::class)
             ->disableOriginalConstructor();
 
-        // HACK: due to differences in NC26 and NC 27
-        if (!method_exists(TimeFactory::class, 'now')) {
-            $timeFactoryBuilder->addMethods(['now'])
-                ->onlyMethods(['getTime']);       
-        } else if(!method_exists(TimeFactory::class, 'getTime')) {
-            $timeFactoryBuilder->addMethods(['getTime']);
-        }
-
-        $timeFactory = $timeFactoryBuilder->getMock();
-        $timeFactory->expects($this->any())
-            ->method('getTime')
-            ->will($this->returnValue($this->time));
-
         $mockDateTime = $this->getMockBuilder(\DateTimeImmutable::class)
             ->disableOriginalConstructor()
             ->getMock();
