9a2252781b99f7f394d4d9d29b208febb295729f
Devlin Junker <devlin.junker@gmail.com>
2023-08-08 22:55:03 -0700

    started on fetching starred items from backend and displaying with for loop
    Signed-off-by: Devlin Junker <devlin.junker@gmail.com>

--- src/types/ApiRoutes.ts
+++ src/types/ApiRoutes.ts
@@ -3,4 +3,5 @@ import { generateUrl } from '@nextcloud/router'
 export const API_ROUTES = {
 	FOLDER: generateUrl('/apps/news/folders'),
 	FEED: generateUrl('/apps/news/feeds'),
+	ITEMS: generateUrl('/apps/news/items'),
 }
