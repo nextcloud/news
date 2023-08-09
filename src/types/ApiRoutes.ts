e559cb35d4fed3c7c38c0dd3fd2d161b077f88af
Devlin Junker <devlin.junker@gmail.com>
2023-08-09 07:59:03 -0700

    fix linting errors
    Signed-off-by: Devlin Junker <devlin.junker@gmail.com>

--- src/types/ApiRoutes.ts
+++ src/types/ApiRoutes.ts
@@ -1,6 +1,6 @@
 import { generateUrl } from '@nextcloud/router'
 
 export const API_ROUTES = {
-  FOLDER: generateUrl('/apps/news/folders'),
-  FEED: generateUrl('/apps/news/feeds')
+	FOLDER: generateUrl('/apps/news/folders'),
+	FEED: generateUrl('/apps/news/feeds'),
 }
