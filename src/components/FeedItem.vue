9157a885c13c746935236a28bcb5113a3c737669
Devlin Junker <devlin.junker@gmail.com>
2023-08-14 21:40:21 -0700

    some cleanup and improvements
    Signed-off-by: Devlin Junker <devlin.junker@gmail.com>

--- src/components/FeedItem.vue
+++ src/components/FeedItem.vue
@@ -1,25 +1,29 @@
 <template>
-	<div style="padding: 5px 10px; display: flex;" @click="expand()">
-		<div style="padding: 0px 5px;">
-			<EarthIcon />
+	<div class="feed-item-container" style="padding: 5px 10px;" @click="expand()">
+		<div style="display: flex;">
+			<div style="padding: 0px 5px;">
+				<EarthIcon />
+			</div>
+			<div style="flex-grow: 1; overflow: hidden; text-overflow: ellipsis;">
+				<span style="text-overflow: ellipsis;" :style="{ 'white-space': !isExpanded ? 'nowrap' : 'normal' }">
+					{{ item.title }}
+				</span>
+			</div>
+			<div class="button-container" style="display: flex; flex-direction: row; align-self: start;">
+				<StarIcon :class="{'starred': item.starred }" />
+				<Eye />
+				<ShareVariant />
+			</div>
 		</div>
-		<div style="flex-grow: 1; overflow: hidden; text-overflow: ellipsis;">
-			<span style="text-overflow: ellipsis;" :style="{ 'white-space': !isExpanded ? 'nowrap' : 'normal' }">
-				{{ item.title }}
-			</span>
-		</div>
-		<div class="button-container" style="display: flex; flex-direction: row;">
-			<StarIcon />
-			<EyeOutline />
-			<ShareVariant />
-		</div>
+
+		<div v-if="isExpanded" v-html="item.body" />
 	</div>
 </template>
 
 <script>
 import EarthIcon from 'vue-material-design-icons/Earth.vue'
 import StarIcon from 'vue-material-design-icons/Star.vue'
-import EyeOutline from 'vue-material-design-icons/EyeOutline.vue'
+import Eye from 'vue-material-design-icons/Eye.vue'
 import ShareVariant from 'vue-material-design-icons/ShareVariant.vue'
 
 export default {
@@ -27,7 +31,7 @@ export default {
 	components: {
 		EarthIcon,
 		StarIcon,
-		EyeOutline,
+		Eye,
 		ShareVariant,
 	},
 	props: {
@@ -54,3 +58,25 @@ export default {
 }
 
 </script>
+
+<style>
+	.feed-item-container, .feed-item-container * {
+		cursor: pointer;
+	}
+
+	.material-design-icon {
+		color: #555555;
+	}
+
+	.material-design-icon:hover {
+		color: var(--color-main-text);
+	}
+
+	.material-design-icon.starred {
+		color: rgb(255, 204, 0);
+	}
+
+	.material-design-icon.starred:hover {
+		color: #555555;
+	}
+</style>
--- src/components/Starred.vue
+++ src/components/Starred.vue
@@ -1,6 +1,9 @@
 <template>
-	<div style="margin-top: 40px;">
-		<VirtualScroll :reached-end="items.starredLoaded">
+	<div>
+		<div style="padding-left: 50px; position: absolute; top: 1em; font-weight: 700;">
+			Starred
+		</div>
+		<VirtualScroll :reached-end="items.starredLoaded" style="margin-top: 50px; border-top: 1px solid var(--color-border);">
 			<template v-if="items.starredItems && items.starredItems.length > 0">
 				<template v-for="item in items.starredItems">
 					<FeedItem :key="item.id" :item="item" />
