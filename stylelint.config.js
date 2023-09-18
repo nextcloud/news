b983c09edbfc53eed437bba732e80a8aec43956c
Devlin Junker <devlin.junker@gmail.com>
2023-09-18 12:23:48 -0700

    ignore vue files for stylelint (probably need to remove old css files soon)
    Signed-off-by: Devlin Junker <devlin.junker@gmail.com>

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,6 @@
 const stylelintConfig = require('@nextcloud/stylelint-config')
 
 stylelintConfig.rules.indentation = 4
+stylelintConfig.ignoreFiles.push('**/*.vue')
 
 module.exports = stylelintConfig
