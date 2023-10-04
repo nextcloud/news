498c001bd35553d68b9332a971fd22afccdfdefa
Devlin Junker <devlin.junker@gmail.com>
2023-10-04 15:16:48 -0700

    cleanup
    Signed-off-by: Devlin Junker <devlin.junker@gmail.com>

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,9 @@ import { Folder } from '../types/Folder'
 import { Feed } from '../types/Feed'
 import feeds, { FEED_ACTION_TYPES } from './feed'
 import folders, { FOLDER_ACTION_TYPES } from './folder'
-import { FOLDER_MUTATION_TYPES, FEED_MUTATION_TYPES, FEED_ITEM_MUTATION_TYPES } from '../types/MutationTypes'
+import { FOLDER_MUTATION_TYPES, FEED_MUTATION_TYPES, FEED_ITEM_MUTATION_TYPES, APPLICATION_MUTATION_TYPES } from '../types/MutationTypes'
 import items, { FEED_ITEM_ACTION_TYPES, ItemState } from './item'
-import app, { APPLICATION_ACTION_TYPES, APPLICATION_MUTATION_TYPES } from './app'
+import app, { APPLICATION_ACTION_TYPES } from './app'
 
 export const MUTATIONS = {
 	...APPLICATION_MUTATION_TYPES,
