069160a57e0e7241e51d5228923844c2c1c35548
Wolfgang <github@linux-dude.de>
2025-05-02 11:08:41 +0200

    vue3-migration: update to @nextcloud/eslint-config v9
    Signed-off-by: Wolfgang <github@linux-dude.de>

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: CC0-1.0
+ */
+import { recommendedLibrary } from '@nextcloud/eslint-config'
+
+export default [
+	...recommendedLibrary,
+]
