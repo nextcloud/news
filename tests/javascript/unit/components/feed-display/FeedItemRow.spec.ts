7dd5d4b32675c6f579bfddc8a3291ad75ede5eab
Wolfgang <github@linux-dude.de>
2025-08-25 18:32:13 +0200

    testing: fix javascript date format test
    Signed-off-by: Wolfgang <github@linux-dude.de>

--- tests/javascript/unit/components/feed-display/FeedItemRow.spec.ts
+++ tests/javascript/unit/components/feed-display/FeedItemRow.spec.ts
@@ -57,11 +57,11 @@ describe('FeedItemRow.vue', () => {
 		const epoch = Date.now() // Provide an epoch timestamp
 		const formattedDate = (wrapper.vm as any).formatDate(epoch / 1000)
 
-		expect(formattedDate).toEqual(new Date(epoch).toLocaleString(undefined, {
+		expect(formattedDate).toEqual(new Date(epoch).toLocaleString(OC.getLanguage(), {
 			year: "numeric",
 			month: "2-digit",
 			day: "2-digit",
-			hour: "2-digit",
+			hour: "numeric",
 			minute: "2-digit",
 			second: "2-digit",
 		}))
