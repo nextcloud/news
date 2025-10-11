1f516cb012b086d608601849a90e9bc94aadf6e6
Wolfgang <github@linux-dude.de>
2025-10-11 23:56:49 +0200

    testing: add unit tests for the recently added routes 'item' and 'recent'
    Signed-off-by: Wolfgang <github@linux-dude.de>

---
+++ tests/javascript/unit/components/routes/Recent.spec.ts
@@ -0,0 +1,60 @@
+import Vuex, { Store } from 'vuex'
+import { shallowMount } from '@vue/test-utils'
+import { beforeAll, beforeEach, describe, expect, it, vi } from 'vitest'
+
+import Recent from '../../../../../src/components/routes/Recent.vue'
+import ContentTemplate from '../../../../../src/components/ContentTemplate.vue'
+
+vi.mock('@nextcloud/axios')
+
+describe('Recent.vue', () => {
+	'use strict'
+	let wrapper: any
+
+	const mockItems = [
+		{
+			id: 1,
+		}, {
+			id: 2,
+		}, {
+			id: 3,
+		}, {
+			id: 4,
+		}
+	]
+
+	const mockItemIds = [ 2, 3 ];
+
+	let store: Store<any>
+	beforeAll(() => {
+		store = new Vuex.Store({
+			state: {
+				items: {
+					allItems: mockItems,
+					recentItemIds: mockItemIds,
+				},
+			},
+			getters: {
+				allItems: () => mockItems,
+				recentItemIds: () => mockItemIds,
+			},
+		})
+
+		store.dispatch = vi.fn()
+		store.commit = vi.fn()
+
+		wrapper = shallowMount(Recent, {
+			global: {
+				plugins: [store],
+			},
+		})
+	})
+
+	beforeEach(() => {
+		vi.clearAllMocks()
+	})
+
+	it('should get recent items from state', () => {
+		expect((wrapper.findComponent(ContentTemplate)).props().items.length).toEqual(2)
+	})
+})
