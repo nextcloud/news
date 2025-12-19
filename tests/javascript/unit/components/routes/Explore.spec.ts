adde23563e550113598ff772be2f4ff8c5a6210e
Wolfgang <github@linux-dude.de>
2025-12-19 19:31:37 +0100

    refactor: remove unused settings routes and functions
    Signed-off-by: Wolfgang <github@linux-dude.de>

--- tests/javascript/unit/components/routes/Explore.spec.ts
+++ tests/javascript/unit/components/routes/Explore.spec.ts
@@ -1,7 +1,9 @@
 import axios from '@nextcloud/axios'
 import { shallowMount } from '@vue/test-utils'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import Explore from '../../../../../src/components/routes/Explore.vue'
+
+vi.mock('@nextcloud/initial-state')
 
 describe('Explore.vue', () => {
 	'use strict'
