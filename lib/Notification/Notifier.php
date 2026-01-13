a31d493a2da55b7d4b63c6f2a72565d7049cd754
leonace924 <meobius123@gmail.com>
2026-01-13 21:22:17 +0100

    feat: add notifications when sharing articles with users
    Users now receive a notification when someone shares an article with
    them. The notification includes:
    - Sharer's display name
    - Article title (truncated if > 100 chars)
    - Clickable link to open the News app
    - News app icon
    
    Implementation details:
    - Added Notifier class implementing INotifier interface
    - Registered notifier in Application.php
    - ShareService sends notification after successful share
    - Notification failures are logged but don't break the share flow
    
    Fixes #2102 (notification part - focus fix was already implemented)
    
    Signed-off-by: eureka928 <meobius123@gmail.com>
    Signed-off-by: Benjamin Brahmer <info@b-brahmer.de>

---
+++ lib/Notification/Notifier.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Nextcloud - News
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ */
+
+namespace OCA\News\Notification;
+
+use OCA\News\AppInfo\Application;
+use OCP\IURLGenerator;
+use OCP\IUserManager;
+use OCP\L10N\IFactory;
+use OCP\Notification\INotification;
+use OCP\Notification\INotifier;
+
+class Notifier implements INotifier
+{
+    private IFactory $l10nFactory;
+    private IURLGenerator $urlGenerator;
+    private IUserManager $userManager;
+
+    public function __construct(
+        IFactory $l10nFactory,
+        IURLGenerator $urlGenerator,
+        IUserManager $userManager
+    ) {
+        $this->l10nFactory = $l10nFactory;
+        $this->urlGenerator = $urlGenerator;
+        $this->userManager = $userManager;
+    }
+
+    public function getID(): string
+    {
+        return Application::NAME;
+    }
+
+    public function getName(): string
+    {
+        return $this->l10nFactory->get(Application::NAME)->t('News');
+    }
+
+    public function prepare(INotification $notification, string $languageCode): INotification
+    {
+        if ($notification->getApp() !== Application::NAME) {
+            throw new \InvalidArgumentException('Unknown app');
+        }
+
+        $l = $this->l10nFactory->get(Application::NAME, $languageCode);
+
+        switch ($notification->getSubject()) {
+            case 'shared_article':
+                return $this->prepareSharedArticle($notification, $l);
+
+            default:
+                throw new \InvalidArgumentException('Unknown subject');
+        }
+    }
+
+    private function prepareSharedArticle(INotification $notification, \OCP\IL10N $l): INotification
+    {
+        $params = $notification->getSubjectParameters();
+        $sharerUserId = $params['sharedBy'] ?? '';
+        $itemTitle = $params['itemTitle'] ?? $l->t('an article');
+
+        $sharerDisplayName = $sharerUserId;
+        $user = $this->userManager->get($sharerUserId);
+        if ($user !== null) {
+            $sharerDisplayName = $user->getDisplayName();
+        }
+
+        $notification->setParsedSubject(
+            $l->t('%1$s shared "%2$s" with you', [$sharerDisplayName, $itemTitle])
+        );
+
+        $notification->setRichSubject(
+            $l->t('{user} shared "{article}" with you'),
+            [
+                'user' => [
+                    'type' => 'user',
+                    'id' => $sharerUserId,
+                    'name' => $sharerDisplayName,
+                ],
+                'article' => [
+                    'type' => 'highlight',
+                    'id' => $notification->getObjectId(),
+                    'name' => $itemTitle,
+                ],
+            ]
+        );
+
+        $notification->setLink(
+            $this->urlGenerator->linkToRouteAbsolute('news.page.index')
+        );
+
+        $notification->setIcon(
+            $this->urlGenerator->getAbsoluteURL(
+                $this->urlGenerator->imagePath(Application::NAME, 'news.svg')
+            )
+        );
+
+        return $notification;
+    }
+}
