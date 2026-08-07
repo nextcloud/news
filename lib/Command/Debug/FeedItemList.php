78b84c5455ab7af3a70a6669164088efd724364d
Benjamin Brahmer <info@b-brahmer.de>
2026-08-07 08:06:27 +0200

    Fix invalid signature
    Signed-off-by: Benjamin Brahmer <info@b-brahmer.de>

--- lib/Command/Debug/FeedItemList.php
+++ lib/Command/Debug/FeedItemList.php
@@ -55,9 +55,9 @@ class FeedItemList extends Command
      * @param InputInterface  $input
      * @param OutputInterface $output
      *
-     * @return int|void
+     * @return int
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $user = $input->getArgument('user-id');
 
